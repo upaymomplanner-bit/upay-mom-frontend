cdfad697dce2a8301695b2b3d23cf857395e9901
prajasnaik <prajadnaik@gmail.com>
2025-12-10 11:53:44 +0530

    add query provider and analytics dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/layout/header";
 import { Sidebar } from "@/components/layout/sidebar";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 export default function DashboardLayout({
   children,
@@ -7,14 +8,16 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full relative min-h-screen bg-gray-50 dark:bg-zinc-900">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <Sidebar />
+    <QueryProvider>
+      <div className="h-full relative min-h-screen bg-gray-50 dark:bg-zinc-900">
+        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+          <Sidebar />
+        </div>
+        <main className="md:pl-72 h-full min-h-screen flex flex-col">
+          <Header />
+          <div className="flex-1 p-8">{children}</div>
+        </main>
       </div>
-      <main className="md:pl-72 h-full min-h-screen flex flex-col">
-        <Header />
-        <div className="flex-1 p-8">{children}</div>
-      </main>
-    </div>
+    </QueryProvider>
   );
 }
