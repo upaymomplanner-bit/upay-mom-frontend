d04c2da0046d68a2fb79b565f8d14c8f7813e6a8
prajasnaik <prajadnaik@gmail.com>
2025-12-13 16:10:09 +0530

    added theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
